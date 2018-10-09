a9531dcced86e42024d65d46ec301d603283e858
Heloísa de Paula Silva Ramalho <ramalho.heloisa@academico.ifrn.edu.br>
2018-10-09 11:07:50 -0300

    Ultima

--- application/controllers/doacao.php
+++ application/controllers/doacao.php
@@ -1,11 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 // Classe doação
-<<<<<<< HEAD
 //  outra modificação
-=======
-// outra classe
->>>>>>> 816eb1844a50e1e88b94419aa3aef4a55181c989
 class doacao extends CI_Controller {
 
 	public function index() {
